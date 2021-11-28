3b85642bff0d0901a882711fb0849b9f5fa25d15
pratik-kadoli <shubham.mitkari@arraypointer.com>
2021-11-28 12:17:30 +0530

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'material';
+  notifications=0;
+  showSpinner=false;
+  opened=false;
+
+}
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { FormsModule } from '@angular/forms';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCardModule } from '@angular/material/card';
+
+const material =[
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  FormsModule,
+  MatMenuModule,
+  MatListModule,
+  MatGridListModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatCardModule
+]
+
+@NgModule({
+  imports: [
+    material,
+  ],
+    
+  exports: [
+    material,
+  ]
+})
+export class MaterialModule { }
